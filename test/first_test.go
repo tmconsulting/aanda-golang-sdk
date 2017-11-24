456cf48524b2f725cf5647b72deeaa7e62a716d5
versoul <mailto.versoul@gmail.com>
2017-11-24 22:10:57 +0300

    example test

--- test/first_test.go
+++ test/first_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestReverseToReturnReversedInputString(t *testing.T) {
 	actualResult := "Hello"
-	var expectedResult = "olleH"
+	var expectedResult = "Hello"
 
 	if actualResult != expectedResult {
 		t.Fatalf("Expected %s but got %s", expectedResult, actualResult)
