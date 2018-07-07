c85f010962770eb7388a0e9da124359ca1195155
alexey <piyanin@gmail.com>
2018-07-07 23:54:46 +0300

    Implementing MustFloat64; fix protocol

--- helper.go
+++ helper.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"strconv"
+	"strings"
+	"errors"
 )
 
 type MustString string
@@ -22,6 +24,34 @@ func (o *MustString) UnmarshalJSON(data []byte) error {
 		*o = MustString(strconv.Itoa(v))
 	default:
 		*o = MustString(fmt.Sprint(v))
+	}
+
+	return nil
+}
+
+type MustFloat64 float64
+
+func (o *MustFloat64) UnmarshalJSON(data []byte) error {
+	var v interface{}
+
+	if err := json.Unmarshal(data, &v); err != nil {
+		return err
+	}
+
+	switch v := v.(type) {
+	case float64:
+		*o = MustFloat64(v)
+	case float32:
+		*o = MustFloat64(v)
+	case int64:
+		*o = MustFloat64(v)
+	case int:
+		*o = MustFloat64(v)
+	case string:
+		f, _ := strconv.ParseFloat(strings.Trim(v,`"`),64)
+		*o = MustFloat64(f)
+	default:
+		return errors.New(fmt.Sprintf("error unmarshal %#v as float64", v))
 	}
 
 	return nil
